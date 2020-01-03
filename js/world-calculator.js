e17cf19de8316a8ba0bf851eb56c0ee85b32d6ba
ITSTAKE <bexco2010@gmail.com>
2020-01-03 21:27:40 +0900

    Some fixes.

--- js/world-calculator.js
+++ js/world-calculator.js
@@ -1,4 +1,5 @@
 function calculate() {
+    document.getElementById("song-table").innerHTML = "<p>Ctrl+F 를 이용하여 원하는 곡을 검색하면 빠르게 찾을 수 있습니다.</p>\n<p>하드게이지 클리어 실패는 0점으로 계산합니다.</p>";
     document.getElementById("step-to-foward-invalid").style = "";
     document.getElementById("step-invalid").style = "";
     document.getElementById("step-to-forward-min").classList.remove("is-invalid");
@@ -47,6 +48,7 @@ function calculate() {
         thead.appendChild(headtr);
         tbl.appendChild(thead);
         var tbody = document.createElement("tbody");
+        var added = false;
         for(var i = 0; i < songs.length; i++) {
             console.log("Reading " + songs[i]["title_localized"]["en"] + "...");
             var charts = songs[i]["difficulties"]
@@ -54,6 +56,7 @@ function calculate() {
                 var score_min = get_score(charts[j]["detailed_rating"], chart_potential_min).toFixed(0);
                 var score_max = get_score(charts[j]["detailed_rating"], chart_potential_max).toFixed(0);
                 console.log("score_min: " + score_min + ", score_max: " + score_max);
+                if(score_min < 0 && score_max < 0) continue;
                 var score_string = "";
                 if(score_min >= 0) {
                     score_string += score_min;
@@ -62,7 +65,10 @@ function calculate() {
                 if(score_max >= 0) {
                     score_string += score_max;
                 }
-                if(score_string == "~") continue;
+                var rating_string = charts[j]["rating"]
+                if(rating_string == 10) rating_string = "9+";
+                if(rating_string == 11) rating_string = "10";
+                added = true;
                 var tr = document.createElement("tr");
                 var tdsn = document.createElement("td");
                 tdsn.appendChild(document.createTextNode(songs[i]["title_localized"]["en"]));
@@ -71,7 +77,7 @@ function calculate() {
                 var tdrColors = ["text-primary", "text-success", "text-danger"];
                 var tdrText = ["PAST ", "PRESENT ", "FUTURE "];
                 tdr.classList.add(tdrColors[j]);
-                tdr.appendChild(document.createTextNode(tdrText[j] + charts[j]["rating"]));
+                tdr.appendChild(document.createTextNode(tdrText[j] + rating_string));
                 tr.appendChild(tdr);
                 var tddr = document.createElement("td");
                 tddr.appendChild(document.createTextNode(charts[j]["detailed_rating"]));
@@ -82,8 +88,18 @@ function calculate() {
                 tbody.appendChild(tr);
             }
         }
+        if(added == false) {
+            var tr = document.createElement("tr");
+            var td = document.createElement("td");
+            td.colSpan = 4;
+            td.style = "padding: 30px 0; text-align: center;";
+            td.appendChild(document.createTextNode("해당 범위만큼 전진할 수 있도록 하는 곡이 존재하지 않습니다. :("));
+            tr.appendChild(td);
+            tbody.appendChild(tr);
+        }
         tbl.appendChild(tbody);
         songdiv.appendChild(tbl);
+        songdiv.style = "display: block;";
     });
 }
 
